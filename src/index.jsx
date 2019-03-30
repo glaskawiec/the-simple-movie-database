f50727554a3c2f53bc2a1cba8d511848738d86e1
Gilbert Łaskawiec <gilbertlaskawiec@gmail.com>
2019-03-30 19:40:31 +0100

    Converted fonts to .woff2

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,8 @@
+import './resources/fonts/index.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { HouxProvider } from 'houx';
 import App from './App';
-import './resources/fonts/index.css';
 import global from './Flux/Reducers/global';
 import discover from './Flux/Reducers/discover';
 import movie from './Flux/Reducers/movie';
