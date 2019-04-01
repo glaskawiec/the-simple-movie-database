3801268a5846c57c991663ef798f418e699390e0
Gilbert Łaskawiec <gilbertlaskawiec@gmail.com>
2019-04-01 02:52:44 +0200

    Routes fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { Suspense } from 'react';
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import { Reset } from 'styled-reset';
 import { useHoux } from 'houx';
@@ -8,7 +8,7 @@ import GlobalStyle from './globalStyle';
 import Discover from './routes/discover/Discover';
 import Themes from './themes';
 import LoadingScreen from './common/loadingScreen/LoadingScreen';
-import NotFound from "./common/notFound/NotFound";
+import NotFound from './common/notFound/NotFound';
 
 const Find = React.lazy(() => import('./routes/find/Find'));
 const Movie = React.lazy(() => import('./routes/movie/Movie'));
@@ -22,14 +22,14 @@ const App = () => {
           <Reset />
           <GlobalStyle />
           <Layout>
-            <Switch>
-            <Route path="/" exact component={Discover} />
             <Suspense fallback={<LoadingScreen />}>
-              <Route path="/find" exact component={Find} />
-              <Route path="/movie/:id/:title" exact component={Movie} />
+              <Switch>
+                <Route path="/" exact component={Discover} />
+                <Route path="/find" component={Find} />
+                <Route path="/movie/:id/:title" component={Movie} />
+                <Route component={NotFound} />
+              </Switch>
             </Suspense>
-            <Route path="*" component={NotFound}/>
-            </Switch>
           </Layout>
         </>
       </ThemeProvider>
