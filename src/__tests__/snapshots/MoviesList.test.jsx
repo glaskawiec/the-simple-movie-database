0c5f25bc33c7ee97c627bfebfcedba6a2057e21d
Gilbert Łaskawiec <gilbertlaskawiec@gmail.com>
2019-04-01 20:29:49 +0200

    Updated snapshot tests

--- src/__tests__/snapshots/MoviesList.test.jsx
+++ src/__tests__/snapshots/MoviesList.test.jsx
@@ -1,21 +1,24 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import { ThemeProvider } from 'styled-components';
+import { MemoryRouter } from 'react-router-dom';
 import themes from '../../themes';
 import MoviesList from '../../common/moviesList/MoviesList';
 
 test('MoviesList snapshot', () => {
   const tree = renderer
     .create(
-      <ThemeProvider theme={themes.light}>
-        <MoviesList
-          currentPage={1}
-          isError={false}
-          isLoading={false}
-          onPageChange={() => {}}
-          totalPages={1}
-        />
-      </ThemeProvider>,
+      <MemoryRouter initialEntries={[{ pathname: '/', key: 'testKey' }]}>
+        <ThemeProvider theme={themes.light}>
+          <MoviesList
+            currentPage={1}
+            isError={false}
+            isLoading={false}
+            onPageChange={() => {}}
+            totalPages={1}
+          />
+        </ThemeProvider>
+      </MemoryRouter>,
     )
     .toJSON();
   expect(tree).toMatchSnapshot();
